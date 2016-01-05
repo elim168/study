9f08de536397679aacf65058bd78b79a326e3b0a
elim <yeelimzhang@163.com>
2016-01-05 22:53:37 +0800

    增加对merge和detach的级联查询。

--- src/test/java/com/elim/learn/jpa/ManyToOneTest.java
+++ src/test/java/com/elim/learn/jpa/ManyToOneTest.java
@@ -63,12 +63,99 @@ public class ManyToOneTest {
 	 */
 	@Test
 	public void testPersistence() {
-		User author = entityManager.getReference(User.class, 2);
+		User author = entityManager.getReference(User.class, 4);
 		Article article = new Article();
 		article.setAuthor(author);
 		article.setTitle("标题-----");
 		article.setContent("这是一篇什么什么样的文章。");
 		entityManager.persist(article);
+	}
+	
+	/**
+	 * 对于单向多对一关联，如果在多的一方通过@ManyToOne的cascade属性指定了级联持久化，则基于Hibernate实现的JPA在持久化多的一方时不会把一的一方也一并持久化。
+	 */
+	@Test
+	public void testCascadePersistence() {
+		//直接new一个User对象
+		User author = new User();
+		author.setName("author abc");
+		author.setAge(36);
+		Article article = new Article();
+		article.setTitle("title for test");
+		article.setContent("content for test");
+		//只持久化多的一方，article
+		entityManager.persist(article);
+	}
+	
+	/**
+	 * 对于单向多对一关联，如果在多的一方通过@ManyToOne的cascade属性指定了级联merge，则基于Hibernate实现的JPA在merge新new出来的多的一方
+	 * 时效果和进行persist操作的结果是类似的，其不会将一的一方也持久化
+	 */
+	@Test
+	public void testCascadeMerge() {
+		//直接new一个User对象
+		User author = new User();
+		author.setName("author abc");
+		author.setAge(36);
+		Article article = new Article();
+		article.setTitle("title for test");
+		article.setContent("content for test");
+		//只持久化多的一方，article
+		entityManager.merge(article);
+		System.out.println("--------------------分界线--------------------");
+	}
+	
+	/**
+	 * 对于单向多对一关联，如果在多的一方通过@ManyToOne的cascade属性指定了级联merge，则基于Hibernate实现的JPA在merge从数据库查询
+	 * 出来的实体对象时即使一的一方发生了改变也不会级联merge，只有在提交事务时才会将修改一并flush到数据库中。
+	 */
+	@Test
+	public void testCascadeMerge2() {
+		Article article = this.entityManager.find(Article.class, 6);
+		article.setTitle("title for test");
+		article.setContent("content for test");
+		article.getAuthor().setName("name for test");
+		//只持久化多的一方，article
+		entityManager.merge(article);
+		System.out.println("--------------------分界线--------------------");
+	}
+	
+	/**
+	 * 对于单向的多对一关联，如果在多的一方通过@ManyToOne的cascade属性指定了级联删除，则基于Hibernate实现的JPA会在删除多的一方时级联的把一的一方也一并删除了。
+	 */
+	@Test
+	public void testCascadeRemove() {
+		Article article = entityManager.find(Article.class, 2);
+		//删除多的一方
+		entityManager.remove(article);
+	}
+	
+	/**
+	 * 对于单向的多对一关联，如果在多的一方通过@ManyToOne的cascade属性指定了级联refresh，则基于Hibernate实现的JPA会在refresh多的一方时把一的一方也一并refresh了。
+	 */
+	@Test
+	public void testCascadeRefresh() {
+		Article article = entityManager.find(Article.class, 6);
+		String prevTitle = article.getTitle();
+		String prevAuthorName = article.getAuthor().getName();
+		article.setTitle(prevTitle + "--reset");
+		article.getAuthor().setName(prevAuthorName + "--reset");
+		//对多的一方进行refresh操作
+		entityManager.refresh(article);
+		System.out.println(prevTitle.equals(article.getTitle()));//true
+		System.out.println(prevAuthorName.equals(article.getAuthor().getName()));//true
+	}
+	
+	/**
+	 * 对于单向的多对一关联，如果在多的一方通过@ManyToOne的cascade属性指定了级联detach，则基于Hibernate实现的JPA会在detach多的一方时把一的一方也从持久化上下文中detach。
+	 */
+	@Test
+	public void testCascadeDetach() {
+		Article article = entityManager.find(Article.class, 6);
+		article.setTitle("abc");
+		article.getAuthor().setName("abc");
+		//从持久化上下文中detach多的一方
+		entityManager.detach(article);
 	}
 	
 	/**
