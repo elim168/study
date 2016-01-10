ba4f8c6890b0dcb1cd5e598f863009824227f537
elim <yeelimzhang@163.com>
2016-01-10 12:18:33 +0800

    调整注释信息，关于@JoinColumn的。

--- src/main/java/com/elim/learn/jpa/entity/Article.java
+++ src/main/java/com/elim/learn/jpa/entity/Article.java
@@ -54,7 +54,9 @@ public class Article {
 	}
 
 	/**
-	 * ManyToOne用于指定多对一关系。ManyToOne等这样的对应关系将使用@JoinColumn注解来指定对应关联的字段的信息，而不能再使用@Column。<br/>
+	 * ManyToOne用于指定多对一关系。ManyToOne等这样的对应关系将使用@JoinColumn注解来指定对应关联的字段的信息，而不能再使用@Column，
+	 * 默认不指定时将会采用属性名加_关联实体主键名的方式作为关联字段，如在此示例中即使不使用@JoinColumn指定关联的字段名，默认也将关联字段命名为author_id，因为关联的属性是author，
+	 * 关联的实体Author的主键名是id。<br/>
 	 * 可以通过@ManyToOne的fetch属性指定一的一方是懒加载还是一开始就加载出来。<br/>
 	 * 可以通过@ManyToOne的cascade属性指定对一的一方的级联形式，CascadeType.ALL表示所有的可级联的操作都将级联
 	 * @return
