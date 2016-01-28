d8f1dfa65ee17ce3c4c7ab0aa56d282381543aa0
elim <yeelimzhang@163.com>
2016-01-29 00:08:23 +0800

    去掉无用的引入项

--- src/test/java/com/elim/learn/jpa/JpqlTest.java
+++ src/test/java/com/elim/learn/jpa/JpqlTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.FetchType;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 import javax.persistence.TemporalType;
