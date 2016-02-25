610c8392ae76a72b3cc77adaf886e65f7012e9ed
elim <yeelimzhang@163.com>
2016-02-26 00:07:01 +0800

    新增Organization实体。

---
+++ src/main/java/com/elim/learn/jpa/spring/data/entity/Organization.java
@@ -0,0 +1,43 @@
+package com.elim.learn.jpa.spring.data.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "t_organization")
+@Entity
+public class Organization {
+
+	private Integer id;
+	private String no;
+	private String name;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNo() {
+		return no;
+	}
+
+	public void setNo(String no) {
+		this.no = no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
