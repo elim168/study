79924625888078c7a2dda6cc7a8ff36a1989c988
elim <yeelimzhang@qq.com>
2016-09-26 16:00:45 +0800

    初体验，初步搭建好对应的环境。

---
+++ src/test/java/com/elim/learn/springdata/redis/test/BasicTest.java
@@ -0,0 +1,36 @@
+package com.elim.learn.springdata.redis.test;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.BoundListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/applicationContext.xml")
+public class BasicTest {
+
+	@Autowired
+	private RedisTemplate<String, String> redisTemplate;
+	
+	@Test
+	public void test() {
+		String key = "testList";
+		BoundListOperations<String, String> boundListOps = redisTemplate.boundListOps(key);
+		Long result = boundListOps.rightPush("ABC");
+		System.out.println(result);
+	}
+	
+	@Test
+	public void test2() {
+		String key = "testList";
+		BoundListOperations<String, String> boundListOps = redisTemplate.boundListOps(key);
+		List<String> elements = boundListOps.range(0, boundListOps.size());
+		System.out.println(elements);
+	}
+	
+}
