8b6039e0b70a5ab79e8a4191c0abb9eae7e61f23
elim <yeelimzhang@qq.com>
2016-09-26 18:39:09 +0800

    新增几个测试

---
+++ src/main/java/com/elim/learn/springdata/redis/model/User.java
@@ -0,0 +1,65 @@
+package com.elim.learn.springdata.redis.model;
+
+public class User {
+
+	private Long id;
+	private String name;
+	private int age;
+
+	public User() {
+		
+	}
+	
+	public User(Long id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age
+	 *            the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
