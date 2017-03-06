fc0084e35370b2429f3d7e4c12d5c9e51dd57ca1
Elim.Zhang <yeelimzhang@163.com>
2017-03-06 20:55:30 +0800

    基于原生API的测试

--- zookeeper/src/main/java/com/elim/learn/zookeeper/App.java
+++ zookeeper/src/main/java/com/elim/learn/zookeeper/App.java
@@ -1,31 +1,25 @@
 package com.elim.learn.zookeeper;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.ZooKeeper.States;
 import org.apache.zookeeper.data.Stat;
 
 /**
  * Hello world!
+ * 使用ZooKeeper自带的客户端
  *
  */
 public class App {
 	public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
 		String connectString = "10.192.48.170:2181,10.192.48.170:2182/elim";
 		Watcher watcher = null;
+		//new了一个ZooKeeper对象后，底层就启动了两个线程，一个是SendThread，一个是EventThread，SendThread就在连接ZooKeeper服务器了
 		ZooKeeper zookeeper = new ZooKeeper(connectString, 3000, watcher);
-		States state = zookeeper.getState();
-		while (state != States.CONNECTED) {
-			TimeUnit.MILLISECONDS.sleep(100);
-			state = zookeeper.getState();
-			System.out.println(state);
-		}
 		create(zookeeper, "/dir-persist", CreateMode.PERSISTENT);
 		create(zookeeper, "/dir-persist-seq", CreateMode.PERSISTENT_SEQUENTIAL);
 		create(zookeeper, "/dir-ephemeral", CreateMode.EPHEMERAL);
@@ -33,6 +27,10 @@ public class App {
 		
 		System.out.println(zookeeper.getChildren("/", watcher));
 		
+		int version = -1;//version为-1将匹配所有的版本号
+		zookeeper.setData("/dir-persist", "ABC".getBytes(), version);
+		byte[] data = zookeeper.getData("/dir-persist", false, null);
+		System.out.println(new String(data));
 	}
 
 	private static void create(ZooKeeper zookeeper, String path, CreateMode mode) throws KeeperException, InterruptedException {
