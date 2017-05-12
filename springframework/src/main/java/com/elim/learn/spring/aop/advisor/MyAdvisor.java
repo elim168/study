252608a187d90cad56207017dcc61a6770e961e4
Elim.Zhang <yeelimzhang@163.com>
2017-05-12 11:48:35 +0800

    新增AutoProxy测试

--- springframework/src/main/java/com/elim/learn/spring/aop/advisor/MyAdvisor.java
+++ springframework/src/main/java/com/elim/learn/spring/aop/advisor/MyAdvisor.java
@@ -6,7 +6,9 @@ package com.elim.learn.spring.aop.advisor;
 import java.lang.reflect.Method;
 
 import org.aopalliance.aop.Advice;
+import org.springframework.aop.ClassFilter;
 import org.springframework.aop.MethodBeforeAdvice;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.PointcutAdvisor;
 
@@ -36,7 +38,42 @@ public class MyAdvisor implements PointcutAdvisor {
 	@Override
 	public Pointcut getPointcut() {
 		//匹配所有的方法调用
-		return Pointcut.TRUE;
+//		return Pointcut.TRUE;
+		return new Pointcut() {
+
+			@Override
+			public ClassFilter getClassFilter() {
+				return ClassFilter.TRUE;
+			}
+
+			@Override
+			public MethodMatcher getMethodMatcher() {
+				return new MethodMatcher() {
+
+					@Override
+					public boolean matches(Method method, Class<?> targetClass) {
+						System.out.println("------matches------" + method.getDeclaringClass().getName() + "." + method.getName());
+						String methodName = method.getName();
+						if ("find".equals(methodName)) {
+							return true;
+						}
+						return false;
+					}
+
+					@Override
+					public boolean isRuntime() {
+						return false;
+					}
+
+					@Override
+					public boolean matches(Method method, Class<?> targetClass, Object[] args) {
+						return false;
+					}
+					
+				};
+			}
+			
+		};
 	}
 
 }
