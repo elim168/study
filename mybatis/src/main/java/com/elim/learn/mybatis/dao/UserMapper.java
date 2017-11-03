fae288c78e74847d3b2779e7f966d577872d3696
Elim.Zhang <yeelimzhang@163.com>
2017-11-03 17:58:55 +0800

    存储过程简单示例。

--- mybatis/src/main/java/com/elim/learn/mybatis/dao/UserMapper.java
+++ mybatis/src/main/java/com/elim/learn/mybatis/dao/UserMapper.java
@@ -4,25 +4,40 @@
 package com.elim.learn.mybatis.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Param;
 
 import com.elim.learn.mybatis.model.User;
 
 /**
- * @author Elim
- * 2016年12月20日
+ * @author Elim 2016年12月20日
  */
 public interface UserMapper {
 
 	User findById(@Param("id") Long id);
-	
+
 	List<User> findByNameAndMobile(@Param("name") String name, String mobile);
-	
+
 	void insert(User user);
+
 	void delete(Long id);
+
 	void update(User user);
-	
+
 	List<User> findAll();
 	
+	/**
+	 * 参数和结果都在params中
+	 * @param params
+	 */
+	void findName(Map<String, Object> params);
+	
+	/**
+	 * 参数和结果都包装为一个User对象
+	 * @param user
+	 * @return
+	 */
+	User findName(User user);
+
 }
