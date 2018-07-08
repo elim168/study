ccf04e5f92d373554a9051a4311ba0d91a11b1f5
Elim <yeelimzhang@163.com>
2018-07-08 09:40:53 +0800

    update

--- springframework/src/main/java/com/elim/learn/spring/mvc/MvcConfiguration.java
+++ springframework/src/main/java/com/elim/learn/spring/mvc/MvcConfiguration.java
@@ -3,15 +3,23 @@
  */
 package com.elim.learn.spring.mvc;
 
-import org.springframework.context.annotation.Bean;
+import java.nio.charset.Charset;
+import java.util.List;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * 验证通过@EnableWebMvc启用SpringMVC支持，该注解的作用类似于中XML文件中
  * 配置的&lt;mvc:annotation-config/>。不用配置文件，使用基于Java类的配置时需要指定DispatcherServlet的
  * contextClass为AnnotationConfigWebApplicationContext，指定contextConfigLocation为
  * 对应的Java配置类的全路径。
+ * 
  * <pre>
  * &lt;servlet>
         &lt;servlet-name>springmvc&lt;/servlet-name>
@@ -30,24 +38,27 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
         &lt;servlet-name>springmvc&lt;/servlet-name>
         &lt;url-pattern>/&lt;/url-pattern>
     &lt;/servlet-mapping>
-    </pre>
-    
+ * </pre>
+ * 
  * 使用了@EnableWebMvc后会自动包含一些默认配置，如果需要自定义一些配置，可以通过实现WebMvcConfigurer接口或者
  * 继承WebMvcConfigurerAdapter覆写对应的方法。
- * @author Elim
- * 2017年11月11日
+ * 
+ * @author Elim 2017年11月11日
  */
-//@Configuration
-//@EnableWebMvc
-//@ComponentScan(basePackages="com.elim.learn.spring.mvc.controller")
-public class MvcConfiguration extends WebMvcConfigurerAdapter {
+// @Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.elim.learn.spring.mvc.controller")
+public class MvcConfiguration  extends WebMvcConfigurerAdapter  {
 
-    @Bean
-    public InternalResourceViewResolver newInternalResourceViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/view/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/view/", ".jsp");
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new MappingJackson2HttpMessageConverter());
+		converters.add(new StringHttpMessageConverter(Charset.defaultCharset()));
+	}
 
 }
