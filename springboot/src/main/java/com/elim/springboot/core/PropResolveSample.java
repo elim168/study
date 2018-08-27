fdcf6b5600fef2cf663d205c1b6ef941ad692fa0
Elim.Zhang <yeelimzhang@163.com>
2018-08-27 13:53:33 +0800

    update

--- springboot/src/main/java/com/elim/springboot/core/PropResolveSample.java
+++ springboot/src/main/java/com/elim/springboot/core/PropResolveSample.java
@@ -2,8 +2,12 @@ package com.elim.springboot.core;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
@@ -13,6 +17,9 @@ import lombok.Data;
 @ConfigurationProperties(prefix="test.prop")
 public class PropResolveSample {
 
+    @Autowired
+    private Environment env;
+    
     @Value("${test.prop.a}")
     private String propA;
     
@@ -23,5 +30,12 @@ public class PropResolveSample {
     private String propC;
     
     private List<String> list;
+    
+    @PostConstruct
+    public void init() {
+        System.out.println(this.env.getProperty("elim.name"));
+        System.out.println(this.env.getProperty("elim.name.default"));
+        System.out.println(this.env.getProperty("elim.name.override"));
+    }
     
 }
