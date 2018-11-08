c6df9810f08576261c4cc8195945cf5b8f5f87e1
Elim.Zhang <yeelimzhang@163.com>
2018-11-08 20:59:32 +0800

    Ribbon Eureka

--- spring-cloud/spring-cloud/spring-cloud-ribbon/src/main/java/com/elim/learn/spring/cloud/ribbon/Application.java
+++ spring-cloud/spring-cloud/spring-cloud-ribbon/src/main/java/com/elim/learn/spring/cloud/ribbon/Application.java
@@ -9,7 +9,7 @@ import com.elim.learn.spring.cloud.ribbon.config.RibbonConfiguration;
 
 @SpringBootApplication
 @RibbonClient(value="hello")
-@RibbonClients(defaultConfiguration=RibbonConfiguration.class)
+//@RibbonClients(defaultConfiguration=RibbonConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
