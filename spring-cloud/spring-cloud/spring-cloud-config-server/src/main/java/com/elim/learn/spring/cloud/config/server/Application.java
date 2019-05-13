5a0d2c09c0216290d59fba916b085d730d29bd9b
Elim.Zhang <yeelimzhang@163.com>
2019-05-13 20:19:21 +0800

    update

--- spring-cloud/spring-cloud/spring-cloud-config-server/src/main/java/com/elim/learn/spring/cloud/config/server/Application.java
+++ spring-cloud/spring-cloud/spring-cloud-config-server/src/main/java/com/elim/learn/spring/cloud/config/server/Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Elim
  * 2019/1/28
@@ -13,6 +17,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class Application {
 
   public static void main(String[] args) {
+    List<String> argList = new ArrayList<>(Arrays.asList(args));
+    argList.add("--spring.config.location=classpath:/");
+    args = argList.toArray(new String[0]);
+
     SpringApplication.run(Application.class, args);
   }
 
