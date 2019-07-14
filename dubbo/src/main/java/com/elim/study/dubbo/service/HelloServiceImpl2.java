9150623b0b61b12b6ec9b4df12c14c6fd501e9d0
Elim <yeelimzhang@163.com>
2019-07-15 00:08:44 +0800

    update

--- dubbo/src/main/java/com/elim/study/dubbo/service/HelloServiceImpl2.java
+++ dubbo/src/main/java/com/elim/study/dubbo/service/HelloServiceImpl2.java
@@ -1,5 +1,6 @@
 package com.elim.study.dubbo.service;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class HelloServiceImpl2 implements HelloService {
@@ -10,12 +11,12 @@ public class HelloServiceImpl2 implements HelloService {
     public void sayHello(String name) {
         System.out.println("Hello 2 " + name + "-----" + counter.incrementAndGet());
         if (counter.get() % 3 != 0) {
-            throw new IllegalArgumentException("Elim Custom Error");
-            /*try {
-//                TimeUnit.MILLISECONDS.sleep(500);
+//            throw new IllegalArgumentException("Elim Custom Error");
+            try {
+                TimeUnit.MILLISECONDS.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }*/
+            }
         }
         System.out.println("Invoke completed");
     }
