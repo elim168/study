48a4a6212c53a173732a3b61b393696adb5dbd32
Elim <yeelimzhang@163.com>
2019-12-13 00:06:43 +0800

    python class

---
+++ python/basic/BagClass.py
@@ -0,0 +1,16 @@
+class Bag:
+    def __init__(self):
+        self.data = []
+
+    def add(self, x):
+        self.data.append(x)
+
+    def addTwice(self, x):
+        self.add(x)
+        self.add(x)
+
+bag = Bag()
+bag.add(1)
+bag.add(10)
+bag.addTwice(100)
+print(bag.data)
