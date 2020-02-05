993a642484109de2d8d277734702cfc0b10f5679
elim <yeelimzhang@163.com>
2020-02-06 00:02:37 +0800

    python concurrent ThreadLocal

---
+++ python/basic/concurrent/thread/test08_threading_thread_local.py
@@ -0,0 +1,30 @@
+# 测试ThreadLocal
+
+import threading
+
+
+local_data = threading.local()	# 类似于Java中的ThreadLocal，多个方法共享同一个ThreadLocal对象，基于该ThreadLocal对象赋予的值将区别于每个线程。
+
+
+def func1(name):
+    local_data.name = name
+    func2()
+
+
+def func2():
+    print('thread-%s  get name from thread local is %s' % (threading.currentThread().getName(), local_data.name))
+
+
+print('main thread name is', threading.currentThread().getName())
+
+t1 = threading.Thread(target=func1, args=('zhangsan',))
+t2 = threading.Thread(target=func1, args=('lisi',))
+t1.start()
+t2.start()
+
+
+'''
+main thread name is MainThread
+thread-Thread-1  get name from thread local is zhangsan
+thread-Thread-2  get name from thread local is lisi
+'''
